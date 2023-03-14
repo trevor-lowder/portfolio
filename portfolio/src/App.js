b9cbed215186faf7edce8cc85fb8d3f9fe297b36
TSL-coding <lowder.trevor@yahoo.com>
2023-03-13 20:12:04 -0500

    add formik, axios, setup component tree

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Projects from './components/Projects/Projects.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home/>
+        <About/>
+        <Projects/>
+        <Contact/>
       </header>
     </div>
   );
