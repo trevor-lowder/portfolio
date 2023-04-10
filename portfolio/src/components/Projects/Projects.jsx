42c34515953d9443d3903c7dc112da2a7990b676
TSL-coding <lowder.trevor@yahoo.com>
2023-04-10 17:55:55 -0500

    update projects page

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ProjectList from "./ProjectList";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Scroll from "../Navbar/Scroll";
 
 const Projects = () => {
   return (
     <>
       <Scroll />
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ display: "flex", justifyContent: "center", flexDirection:"column", alignItems:"center" }}>
+        <Typography sx={{margin:"30px"}}>
+          <h2>
+            Click to see the repo/live site for my recent projects
+          </h2>
+        </Typography>
         <ProjectList />
       </Box>
     </>
