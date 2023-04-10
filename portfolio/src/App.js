b2ebd2a98cd4e796c60334d0783dd22fc227f6ef
TSL-coding <lowder.trevor@yahoo.com>
2023-04-10 14:03:01 -0500

    create deployment branch

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Projects from "./components/Projects/Projects.jsx";
@@ -7,15 +7,15 @@ import NavBar from "./components/Navbar/NavBar.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar/>
+    <HashRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
